928076a7faeb3dae67dbf8d511ad073b05dc9ebb
Prasad H L Bhat <prasad.bhat@ti.com>
2015-01-06 04:24:19 +0530

    Added CMake config files for find_package()

---
+++ common.cmake
@@ -0,0 +1,30 @@
+FUNCTION(create_cmake_config package targets component)
+# Add all targets to the build-tree export set
+export(TARGETS ${targets}
+  FILE "${PROJECT_BINARY_DIR}/${package}Targets.cmake")
+ 
+# Export the package for use from the build-tree
+# (this registers the build-tree with a global CMake-registry)
+#export(PACKAGE FooBar)
+ 
+# Create the FooBarConfig.cmake and FooBarConfigVersion files
+set(REL_INCLUDE_DIR "cmake include")
+# ... for the build tree
+set(CONF_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}" "${PROJECT_BINARY_DIR}")
+configure_file(${package}Config.cmake.in "${PROJECT_BINARY_DIR}/${package}Config.cmake" @ONLY)
+# ... for the install tree
+#set(CONF_INCLUDE_DIRS "\${${package}_CMAKE_DIR}/${REL_INCLUDE_DIR}")
+#configure_file(${package}Config.cmake.in "${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/${package}Config.cmake" @ONLY)
+# ... for both
+configure_file(${package}ConfigVersion.cmake.in "${PROJECT_BINARY_DIR}/${package}ConfigVersion.cmake" @ONLY)
+ 
+# Install the FooBarConfig.cmake and FooBarConfigVersion.cmake
+install(FILES
+  "${PROJECT_BINARY_DIR}/${package}Config.cmake"
+  "${PROJECT_BINARY_DIR}/${package}ConfigVersion.cmake"
+  DESTINATION lib/cmake COMPONENT ${component})
+ 
+# Install the export set for use with the install-tree
+install(EXPORT ${package}Targets DESTINATION
+  lib/cmake COMPONENT ${component})
+ENDFUNCTION()
