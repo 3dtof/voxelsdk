33dbcc81867e0bbc31d84ab6e377c1043a0c4492
Prasad H L Bhat <prasad.bhat@ti.com>
2015-01-05 18:31:21 +0530

    Release configuration set in the shell scripts for Windows

--- App/pclviewer-win.sh
+++ App/pclviewer-win.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-export PATH="$PWD/Voxel/Debug":$PATH
+export CONF="Release"
+
+export PATH="$PWD/Voxel/$CONF":$PATH
 export PATH="/C/Users/x0227529/Documents/PCL 1.7.2/bin":$PATH
 export PATH="/C/Users/x0227529/Documents/PCL 1.7.2/3rdParty/Boost/lib64-msvc-12.0":$PATH
 export PATH="/C/Users/x0227529/Documents/PCL 1.7.2/3rdParty/FLANN/bin":$PATH
 export PATH="/C/Users/x0227529/Documents/PCL 1.7.2/3rdParty/Qhull/bin":$PATH
-export PATH="/C/Program Files/VTK/bin":$PATH
-export VOXEL_FW_PATH="$PWD/TI3DToF/Debug"
-export VOXEL_CONF_PATH="$PWD/TI3DToF/Debug"
-export VOXEL_LIB_PATH="$PWD/TI3DToF/Debug"
+export PATH="/C/Users/x0227529/Documents/PCL 1.7.2/3rdParty/VTK/bin":$PATH
+export VOXEL_FW_PATH="$PWD/TI3DToF/$CONF"
+export VOXEL_CONF_PATH="$PWD/TI3DToF/$CONF"
+export VOXEL_LIB_PATH="$PWD/TI3DToF/$CONF"
 
-App/Debug/SimpleVoxelViewer
+App/$CONF/SimpleVoxelViewer
