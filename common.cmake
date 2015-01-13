bec18aad16ca4d0328e0885b6cb6b4217333f783
Prasad H L Bhat <prasad.bhat@ti.com>
2015-01-13 14:26:29 +0530

    Minor fix for find_package() to work on Ubuntu

--- common.cmake
+++ common.cmake
@@ -18,13 +18,28 @@ configure_file(${package}Config.cmake.in "${PROJECT_BINARY_DIR}/${package}Config
 # ... for both
 configure_file(${package}ConfigVersion.cmake.in "${PROJECT_BINARY_DIR}/${package}ConfigVersion.cmake" @ONLY)
  
+if(WINDOWS)
+
 # Install the FooBarConfig.cmake and FooBarConfigVersion.cmake
 install(FILES
-  "${PROJECT_BINARY_DIR}/${package}Config.cmake"
-  "${PROJECT_BINARY_DIR}/${package}ConfigVersion.cmake"
+    "${PROJECT_BINARY_DIR}/${package}Config.cmake"
+    "${PROJECT_BINARY_DIR}/${package}ConfigVersion.cmake"
   DESTINATION lib/cmake COMPONENT ${component})
- 
+  
 # Install the export set for use with the install-tree
 install(EXPORT ${package}Targets DESTINATION
   lib/cmake COMPONENT ${component})
+  
+elseif(LINUX)
+# Install the FooBarConfig.cmake and FooBarConfigVersion.cmake
+install(FILES
+    "${PROJECT_BINARY_DIR}/${package}Config.cmake"
+    "${PROJECT_BINARY_DIR}/${package}ConfigVersion.cmake"
+  DESTINATION lib/${package} COMPONENT ${component})
+  
+# Install the export set for use with the install-tree
+install(EXPORT ${package}Targets DESTINATION
+  lib/${package} COMPONENT ${component})
+endif()
+ 
 ENDFUNCTION()
