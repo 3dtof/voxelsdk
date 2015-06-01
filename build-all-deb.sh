f4648d59f556db8a6aa6c4763c250b431ce3589b
Prasad H L Bhat <prasad.bhat@ti.com>
2015-06-01 17:03:03 +0530

    Minor DEB script fix

--- build-all-deb.sh
+++ build-all-deb.sh
@@ -9,11 +9,11 @@ version=$1
 
 mkdir -p ../packages/$version
 cd build
+rm -rf doc
 touch ../Voxel/SWIG/Voxel.i
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make clean
 make -j2
-rm -rf doc
 . make_deb.sh
 sudo dpkg -i *$version*.deb
 cp *$version*.deb ../../packages/$version/
