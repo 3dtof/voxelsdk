6dfa3c1ba076341f94a4980a5f07d3e8c2a2dbd6
Prasad H L Bhat <prasad.bhat@ti.com>
2015-11-06 21:11:34 +0530

    Modifications to support installation to work without environment variables set explicitly in system/user. Also, VoxelCommon related stuff is completely removed now.

--- App/SimpleVoxelViewer.cmd
+++ App/SimpleVoxelViewer.cmd
@@ -2,4 +2,4 @@ cd %~dp0
 set VOXEL_SDK_PATH=%~dp0\..
 set PATH=%~dp0;%~dp0\..\lib;%PATH%
 set PYTHONPATH=%~dp0\..\lib\python2.7;%PYTHONPATH%
-START /MAX cmd.exe /C "mode con cols=160 lines=500 && VoxelCLI.exe"
+START /MAX cmd.exe /C "mode con cols=160 lines=500 && SimpleVoxelViewer.exe"
