1177d9ca431ee2fd9a1c02a2e3488c63d300821b
S-Fichtl <s.fichtl@outlook.com>
2019-04-23 18:19:57 +0200

    minor improvements

--- build-all-deb.sh
+++ build-all-deb.sh
@@ -7,7 +7,6 @@ fi
 
 version=$1
 
-mkdir -p ../packages/$version
 mkdir -p build; cd build
 rm -rf doc
 
@@ -15,9 +14,6 @@ if [ -e ../Voxel/SWIG/Voxel.i ]; then touch ../Voxel/SWIG/Voxel.i; fi
 
 cmake -DCMAKE_BUILD_TYPE=Release -DGENERATE_PYTHON_BINDINGS=FALSE ..
 make clean
-make -j2
+make -j`nproc`
 . make_deb.sh
 sudo dpkg -i *$version*.deb
-cp *$version*.deb ../../packages/$version/
-cd ../../packages/$version
-tar -cvzf libvoxel-$version-all.tgz *.deb
